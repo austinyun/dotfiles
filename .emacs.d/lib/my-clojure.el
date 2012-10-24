57a804cff3d4874db76bb99e7082313ac3c252bc
Austin Yun <w.austin.yun@gmail.com>
2012-10-23 19:22:32 -0700

    Remove cruft

--- .emacs.d/lib/my-clojure.el
+++ .emacs.d/lib/my-clojure.el
@@ -19,41 +19,6 @@
 (add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
 (add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-;; More syntax coloring
-(defun tweak-clojure-syntax (mode)
-  (mapcar (lambda (x) (font-lock-add-keywords mode x))
-          '((("#?['`]*(\\|)" . 'clojure-parens))
-            (("#?\\^?{\\|}" . 'clojure-braces))
-            (("\\[\\|\\]" . 'clojure-brackets))
-            ((":\\w+" . 'clojure-keyword))
-            (("#?\"" 0 'clojure-double-quote prepend))
-            (("nil\\|true\\|false\\|%[1-9]?" . 'clojure-special))
-            (("(\\(\\.[^ \n)]*\\|[^ \n)]+\\.\\|new\\)\\([ )\n]\\|$\\)" 1
-              'clojure-java-call))
-            (("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 'font-lock-warning-face t))
-            (("(\\(fn\\>\\)" 0 (progn (compose-region (match-beginning 1)
-                                                      (match-end 1) "ƒ") nil)))
-            (("(\\(->\\>\\)" 0 (progn (compose-region (match-beginning 1)
-                                                      (match-end 1) "→") nil)))
-            (("(\\(->>\\>\\)" 0 (progn (compose-region (match-beginning 1)
-                                                       (match-end 1) "↠") nil)))
-            (("^[a-zA-Z0-9-.*+!_?]+?>" . 'slime-repl-prompt-face)))))
-
-;; Macro for face definition
-(defmacro defcljface (name color desc &optional others)
-  `(defface ,name '((((class color)) (:foreground ,color ,@others)))
-     ,desc :group 'faces))
-
-;; Define extra clojure faces
-(defcljface clojure-parens       "DimGrey"   "Clojure parens")
-(defcljface clojure-braces       "#49b2c7"   "Clojure braces")
-(defcljface clojure-brackets     "#0074e8"   "Clojure brackets")
-(defcljface clojure-keyword      "#45b8f2"   "Clojure keywords")
-(defcljface clojure-namespace    "#a9937a"   "Clojure namespace")
-(defcljface clojure-java-call    "#7587a6"   "Clojure Java calls")
-(defcljface clojure-special      "#0074e8"   "Clojure special")
-(defcljface clojure-double-quote "#00920A"   "Clojure special")
-
 (autoload 'clojure-mode "clojure-mode")
 (add-hook 'clojure-mode-hook
           (lambda ()
